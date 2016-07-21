8e28a3ed35cb22c1da7a0e813c7154b0f6042ac5
Joel Worsham <joel.worsham@gmail.com>
2016-07-21 10:57:53 -0400

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name:       Test Github UPdater
+Plugin URI:        https://github.com/joelworsham/test-github-updater-plugin
+Description:       Testing it out.
+Version:           1.0.0
+GitHub Plugin URI: https://github.com/joelworsham/test-github-updater-plugin
+*/
